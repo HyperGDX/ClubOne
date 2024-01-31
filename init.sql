0ae528713dfb0bcd840164798fbe71bdaf1500b2
TobeNext <1145252363@qq.com>
2024-01-31 15:28:09 +0800

    fix: init docker

--- init.sql
+++ init.sql
@@ -2,6 +2,39 @@
 CREATE DATABASE IF NOT EXISTS co_forum;
 USE co_forum;
 
+-- 删除名为posts的表（如果存在）
+DROP TABLE IF EXISTS posts;
+
+-- 创建新的posts表
+create table if not exists posts (
+    id integer not null primary key auto_increment,
+    user_id integer not null,
+    channel_id varchar(255) not null,
+    title varchar(255) not null,
+    content varchar(255) default null,
+    pics JSON default null,
+    status tinyint not null default 1,
+    like_count int not null default 0,
+    view_count int not null default 1,
+    create_time datetime not null default current_timestamp,
+    update_time datetime not null default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+    delete_time datetime default null
+)engine=innodb default charset=utf8;
+
+-- 插入 posts 表的数据
+INSERT INTO posts (user_id, channel_id, title, content, status)
+VALUES
+(1, '1', 'Title1', 'Content1', 1),
+(2, '1', 'Title2', 'Content2', 1),
+(3, '1', 'Title3', 'Content3', 1),
+(4, '1', 'Title4', 'Content4', 1),
+(5, '1', 'Title5', 'Content5', 1);
+
+
+-- 创建名为co_forum的数据库
+CREATE DATABASE IF NOT EXISTS co_account;
+USE co_account;
+
 DROP TABLE IF EXISTS users;
 
 create table if not exists users (
@@ -31,32 +64,3 @@ VALUES
 (3, 'Charlie', 0, 1, 0, 35, 'password3', 'Region3', '12345678903', 'charlie@example.com'),
 (4, 'David', 0, 1, 1, 40, 'password4', 'Region4', '12345678904', 'david@example.com'),
 (5, 'Eve', 0, 1, 1, 45, 'password5', 'Region5', '12345678905', 'eve@example.com');
-
--- 删除名为posts的表（如果存在）
-DROP TABLE IF EXISTS posts;
-
--- 创建新的posts表
-create table if not exists posts (
-    id integer not null primary key auto_increment,
-    user_id integer not null,
-    foreign key (user_id) references users(id),
-    channel_id varchar(255) not null,
-    title varchar(255) not null,
-    content varchar(255) default null,
-    pics JSON default null,
-    status tinyint not null default 1,
-    like_count int not null default 0,
-    view_count int not null default 1,
-    create_time datetime not null default current_timestamp,
-    update_time datetime not null default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
-    delete_time datetime default null
-)engine=innodb default charset=utf8;
-
--- 插入 posts 表的数据
-INSERT INTO posts (user_id, channel_id, title, content, status)
-VALUES
-(1, '1', 'Title1', 'Content1', 1),
-(2, '1', 'Title2', 'Content2', 1),
-(3, '1', 'Title3', 'Content3', 1),
-(4, '1', 'Title4', 'Content4', 1),
-(5, '1', 'Title5', 'Content5', 1);
